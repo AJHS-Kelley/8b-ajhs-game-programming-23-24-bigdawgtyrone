3af4929034b94a424257eee90fc36c82384da9f7
bigdawgtyrone <burnedtoast48@gmail.com>
2023-10-25 13:09:59 -0400

    augh

---
+++ gameProgramming/gaming_exercises/collections/8B/03b_functions.py
@@ -0,0 +1,59 @@
+#FUNCTION -- a named piece of code that can be reused easily.
+import random
+# FUNCTION SIGNATURE -- Syntax for creating a new function.
+def exampleFunctionA(): # NO PARAMETERS
+    count = 0
+    num = int(input("How many times do you want to wish a happy birthday?\n"))
+    while count < num:
+        print("Happy Birthday!\n")
+        count += 1
+
+def exampleFunctionB(num, count): # PARAMETERS
+    while count < num:
+        print("Happy Birthday!\n")
+        count += 1
+
+# IF A FUNCTION REQUIRES PARAMETERS, YOUR CODE WILL CRASH WITHOUT THEM
+    
+# RUNNING A FUNCTION IS KNOWN AS CALLING A FUNCTION!
+# exampleFunctionA()
+# exampleFunctionB(5,0)
+
+def rollDice(numDice, sizeDice):
+    numRolled = 0
+    sum = 0
+    while numRolled < numDice:
+        roll = random.randint(1, sizeDice)
+        sum += roll
+        print(f"Roll: {roll}\n")
+        numRolled += 1
+        return sum #return will IMMEDIATELY exit a loop, function if/elif/else block.
+        
+rollDice(3,6)
+# rollDice(1,20)
+
+strengthPlayer = rollDice(3,6)
+dexterityPlayer = rollDice,6
+wisdomPlayer = rollDice(3,6)
+
+print(strengthPlayer)
+print(dexterityPlayer)
+print(wisdomPlayer)  
+
+def genStats():
+    playerStats = [0,
+        0, # STRENGTH
+        0, # DEXTERITY
+        0, # CONSTITUTION
+        0, # CONSTITUATION
+        0, # WISDOM
+        0 # CHARISMA
+    ]
+    i = 0
+    while i < len(playerStats):
+        playerStats[i] = rollDice(3, 6) # STRENGTH
+        i += 1
+
+    print(playerStats)
+
+    genStats()
