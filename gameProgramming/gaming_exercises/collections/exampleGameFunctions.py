a1b3623dfa61402db750c1e73c605b2738276993
bigdawgtyrone <burnedtoast48@gmail.com>
2023-11-13 09:43:37 -0500

    Create exampleGameFunctions.py

---
+++ gameProgramming/gaming_exercises/collections/exampleGameFunctions.py
@@ -0,0 +1,28 @@
+# Example Game Functions project, Christian ortiz, v0.0
+import random
+
+def functionOne(""):
+    pass
+
+def functionTwo(param1):
+    pass
+
+def functionThree(param1 = "Default Value"):
+    pass
+
+def functionFour(param1, param2, param3):
+    pass
+
+def powerPunch(speed, power, target):
+    if power >= 9 and speed >= 6 and target == head:
+    knockDown = True
+    knockOut = True
+    print('You landed a perfect punch and knocked your opponent out!')
+    elif power >= 7 and speed >= 6 and target == head:
+        knockDown = True
+        print('You have knocked your opponent down!')
+    else
+        knockdown = False
+        print('You did not land your power punch.')
+
+powerPunch(9, 7, head):
