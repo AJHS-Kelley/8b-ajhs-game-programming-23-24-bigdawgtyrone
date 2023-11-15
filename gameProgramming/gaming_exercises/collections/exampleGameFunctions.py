d388aae76a52cbad52b98dfdb000fa12d4c875f2
bigdawgtyrone <burnedtoast48@gmail.com>
2023-11-15 09:16:28 -0500

    little bit more

--- gameProgramming/gaming_exercises/collections/exampleGameFunctions.py
+++ gameProgramming/gaming_exercises/collections/exampleGameFunctions.py
@@ -25,19 +25,34 @@ def powerPunch(speed, power, target):
         knockDown = True
         print('You have knocked your opponent down!')
     else
-        knockdown = False
+        knockDown = False
         print('You did not land your power punch.')
-        pass
+        
 
     
     
     print('Where do you want to hit your opponent?\n')
 powerPunch = input('Type "head" or "body" then press Enter')
 
+
+playerName = input("What's your fighter name?\nType it here then press enter.\n")
+print(f"you want to be called {playerName}.  that Right?")
+isCorrect = input("Please type yes if correct, no if not correct.\n")
+if isCorrect == "yes":
+    print(f"ok {playerName}, let's get started\n")
+else:
+    playerName = input("what's your fighter name?\nType your name then press enter.\n")
+    
+
 if powerPunch == "head":
     punchPower = random.randint(1, 10)
-    punchSpeed = random.randint(1, 10) 
+    punchSpeed = random.randint(1, 10)
     print('You swung for your opponents head')
-    print('This is your punch power -->' + punchPower)
-    print('This is your punch speed -->' + punchSpeed)
-
+    print(f'This is your punch power--> {punchPower}')
+    print(f'This is your punch speed--> {punchSpeed}')
+elif powerPunch == 'body':
+    punchPower = random.randint(1, 10)
+    punchSpeed = random.randint(1, 10)
+    print('You threw a shot at your opponents body')
+    print(f'This is your punch power--> {punchPower}')
+    print(f'This is your punch speed--> {punchSpeed}')
