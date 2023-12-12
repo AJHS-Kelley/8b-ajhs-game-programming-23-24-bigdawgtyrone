d14be30ce33405da0109354d412711d4ba9dd368
bigdawgtyrone <burnedtoast48@gmail.com>
2023-12-12 14:33:10 -0500

    augh
    i'm only human after all

--- gameProgramming/gaming_exercises/collections/06_OOP/oop.py
+++ gameProgramming/gaming_exercises/collections/06_OOP/oop.py
@@ -5,6 +5,8 @@ class Person: # PascalCase for ClassNames
         self.name = name
         self.age = age
         self.weight = weight
+        self.weakness = None
+        self.nemesis = None
 
 # To String Function -- How the object appears as a string.
     def __str__(self):
@@ -19,14 +21,33 @@ person2 = Person("ChargoggagoggmanchauggagoggchaubunagungamauggChargoggagoggmanc
 print(person1)
 print(person2)
 
-if person1.weight > person2.weight:
-    print(f"{person1.name} weighs more than {person2.name}.")
-elif person1.weight == person2.weight:
-    print("Each person weighs the same.\n")
-else:
-    print(f"{person2.name} weighs more than {person1.name}.")
+# if person1.weight > person2.weight:
+#     print(f"{person1.name} weighs more than {person2.name}.")
+# elif person1.weight == person2.weight:
+#     print("Each person weighs the same.\n")
+# else:
+#     print(f"{person2.name} weighs more than {person1.name}.")
 
-if person1.age < person2.age:
-    print(f"{person1.name} is younger than {person2.name}")
+# if person1.age < person2.age:
+#     print(f"{person1.name} is younger than {person2.name}")
 
-person1.classFunction()
+# person1.classFunction()
+
+# Changing properties After Creation
+print(person1.name)
+person1.name = "goku instinct segregational alliance of the ultimate core crushing scp foundation"
+print(person1.name)
+
+# deleting properties -- danger will robinson, DANGER!
+# This is does not 'reset' a property, it deletes it completely.
+print(person1.name)
+del person1.name 
+# print(person1)
+
+# deleting objects -- delete them if your finished with them.
+del person1
+
+# adding properties to objects
+person2.weakness = "dying"
+print(person2)
+print(person2.weakness)
