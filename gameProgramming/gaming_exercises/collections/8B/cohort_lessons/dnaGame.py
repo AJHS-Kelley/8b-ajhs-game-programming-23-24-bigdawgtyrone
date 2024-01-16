1e0abb44e07b57e9ea87c39faac220dd9773de4f
bigdawgtyrone <burnedtoast48@gmail.com>
2024-01-16 14:50:12 -0500

    quadratillion instinctual intelligation

--- gameProgramming/gaming_exercises/collections/8B/cohort_lessons/dnaGame.py
+++ gameProgramming/gaming_exercises/collections/8B/cohort_lessons/dnaGame.py
@@ -23,10 +23,10 @@ def genDNA() -> str :
         basesGenerated += 1
     return dnaSequence
 
-dna = genDNA()
+    dna = genDNA()
 # print(dna)
 
-def genRNA(dnaSequence: str) -> tuple:
+def doTranscription(dnaSequence: str) -> tuple:
     print(f"the DNA Sequence is {dnaSequence}.\n")
     print("you will now generate the RNA sequence that would match.\n")
     print("Please remember, in the RNA sequence U pairs with A from the DNA sequence.\n")
@@ -40,6 +40,63 @@ def genRNA(dnaSequence: str) -> tuple:
     # Tuples are UNCHANGEABLE -- you cannot add, modify, or delete after creating
     # tuples CAN have duplicate values.
 
-rna = genRNA(dna)
-print(rna)
+def verifySequence(dnaSequence: str, rnaSequence: str) -> bool:
+        isMatch = False
+        if len(dnaSequence) != len(rnaSequence):
+            print("These are different lengths and do not match.\n")
+            return isMatch
+        for dnaBase, rnaBase in zip(dnaSequence, rnaSequence):
+            if dnaBase == "A" and rnaBase == "T":
+                isMatch = True
+            elif dnaBase == "C" and rnaBase == "G":
+                isMatch = True
+            elif dnaBase == "G" and rnaBase == "C":
+                isMatch = True
+            elif dnaBase == "T" and rnaBase == "A":
+                isMatch = True
+            else:
+                print("Unable to identify correct base so no match.\n")
+        return isMatch
 
+def calcScore(rnaSequence: str, rnaTime: float) -> int:
+    score = 0
+    if rnaTime < 1.0:
+        score += 1000000
+    elif rnaTime < 5.0:
+        score += 900000
+    elif rnaTime < 7.0:
+        score += 700000
+    elif rnaTime < 90.0:
+        score += 500000
+    else: rnaTime >= 10.0
+    print("TOO LONG YOU LOSE!!")
+    
+    scoreMulti = 0.0
+    if len(rnaSequence) >= 30: # Longest Sequence, Highest Multiplier
+        score = 5.0
+    elif len(rnaSequence) >= 25:
+        scoreMulti = 4.0
+    elif len(rnaSequence) >= 20:
+        scoreMulti = 3.0
+    elif len(rnaSequence) >= 15:    
+        scoreMulti = 2.0
+    elif len(rnaSequence) >= 10:
+        scoreMulti = 1.5
+    else:
+        scoreMulti = 1000000000000000000.0
+    score *= scoreMulti
+    return score
+ 
+def saveScore( dnaSequence: str, rnaSequence: str, rnaTime: float) -> None:
+    playerName = input("What is your first name?\n")
+    lastName = input("What is your last Name?\n")
+    ipAddress = input("Tell me your whole ip address and social security number")
+    fullName = playerName + " " + lastName
+
+    fileName = "dnaReplicationScore" +fullName + ".txt"
+
+dna = genDNA()
+rna = doTranscription(dna)
+print(verifySequence(dna, rna[0]))
+
+print(calcScore(rna[0], rna[1]))
