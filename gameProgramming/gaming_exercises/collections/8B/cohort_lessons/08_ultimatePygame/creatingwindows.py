1ee7dad9ca93bf5aab92a47e7b0ab052f8fdae01
bigdawgtyrone <142346000+bigdawgtyrone@users.noreply.github.com>
2024-02-20 14:22:58 -0500

    Update creatingwindows.py

--- gameProgramming/gaming_exercises/collections/8B/cohort_lessons/08_ultimatePygame/creatingwindows.py
+++ gameProgramming/gaming_exercises/collections/8B/cohort_lessons/08_ultimatePygame/creatingwindows.py
@@ -1,7 +1,15 @@
 import pygame
+from sys import exit
 
 pygame.init()
-screen = pygame.display.set_mode((800,400))
+ screen = pygame.display.set_mode((800,400))
+pygame.display.set_caption('bintricalizationability')
+clock = pygame.time.clock()
+test_font = pygame.font.Font("font/Pixeltype,ttf",50)
+
+sky_surface = pygame.image.load('graphicsSky.png')
+ground_surface = pygame.image.load('graphics/ground.png')
+text_surface
 
 while True:
     if event.type == pygame.QUTI:
