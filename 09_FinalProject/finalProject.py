7d9a52cd7d90638c9d10e7fedd090914d1dd3d3b
bigdawgtyrone <burnedtoast48@gmail.com>
2024-03-27 14:53:42 -0400

    s

--- 09_FinalProject/finalProject.py
+++ 09_FinalProject/finalProject.py
@@ -1,1 +1,13 @@
 #Final Project, Christian Ortiz, v0.0s
+import sys, random, pygame
+
+resolution = 0  # 0 = low resolution(800, 600), 1 = high resolution (1920, 1080).
+
+screen = pygame.display.set_mode((x, y))
+
+if resolution == 0:
+    x = 800
+    y = 600
+else:
+    x = 1920
+    y = 1080 
