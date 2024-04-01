bc2a95dba3ab88a4020c73d86df645ad5a7aba48
bigdawgtyrone <burnedtoast48@gmail.com>
2024-04-01 13:52:40 -0400

    yuhhhhh

--- 09_FinalProject/finalProject.py
+++ 09_FinalProject/finalProject.py
@@ -3,11 +3,20 @@ import sys, random, pygame
 
 resolution = 0  # 0 = low resolution(800, 600), 1 = high resolution (1920, 1080).
 
-screen = pygame.display.set_mode((x, y))
-
 if resolution == 0:
     x = 800
     y = 600
 else:
     x = 1920
     y = 1080 
+
+pygame.init()
+
+difficulty = int(input("Please type the exact name of the level you would like to play.\n"))
+
+if difficulty == 'Stereo Madness':
+    pygame.display.set_caption('Stereo Madness')
+else:
+    pygame.display.set_caption('Lobotomy wave')
+
+screen = pygame.display.set_mode((x, y))
---
+++ gameProgramming/10_kattis/abc.py
@@ -0,0 +1,21 @@
+#input the integers
+# .split() the 3 integers into seperate variables
+# cast the 3 variables into integers
+# assign correct values from least to greatest
+# A < B < C
+
+integers = input()
+a, b, c, = integers.split()
+a = int(a)
+b = int(b)
+c = int(c)
+
+print(f"a: {a} b: {b} c: {c}")
+if a >= b:
+    a, b = b, a
+if b >= c:
+    b, c = c, b
+if b <= a:
+    b, a = a, b
+
+print(f"a: {a} b: {b} c: {c}")
