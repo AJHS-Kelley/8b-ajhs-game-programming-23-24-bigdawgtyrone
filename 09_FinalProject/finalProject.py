0717b5d0af04654c9fe05f6f93ce5aa38bde851e
bigdawgtyrone <burnedtoast48@gmail.com>
2024-05-13 14:23:46 -0400

    i just edited a lot

--- 09_FinalProject/finalProject.py
+++ 09_FinalProject/finalProject.py
@@ -1,13 +1,20 @@
 import pygame
 from sys import exit
-screen = pygame.display.set_mode((800,400))
-# pygame.font.Font('')
-pygame.display.set_caption('lobotomy dash')
-clock = pygame.time.Clock()
 
+# START SCREEN
+#font = pygame.font.Font('Seagram.ttf', 30)
+screen = pygame.display.set_mode((800,400))
+screen2 = False
+screen3 = False
+pygame.display.set_caption('lobotomy dash')
+game_active = False
 background = pygame.image.load('gdbackground_V1.png')
+#start_wordsurface = font.render('Lobotomy dash', False, 'GREEN')
 playbutton = pygame.image.load('playbutton_V1.png')
-# character = pygame.image.load('newlobotomy.png')
+play_rect = playbutton.get_rect(center = (285,100))
+
+#level_icon = pygame.image.load('newlobotomy.png')
+clock = pygame.time.Clock()
 
 while True:
     for event in pygame.event.get():
@@ -17,20 +24,22 @@ while True:
 
     screen.blit(background,(0,0))
     screen.blit(playbutton,(285,100))
-    #screen.blit(character,(300,100))
+    #screen.blit(level_icon,(300,100))
 
+
+    
+
+
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        if play_rect.collidepoint(event.pos):
+            game_active = True
+    if game_active == True:
+        screen = screen2
+        screen.blit(background, (0,0))
+        screen.blit('newlobotomy.png' (300,130))
+        #screen.blit()
+        #screen.blit()
+        level_name = font.render('Lobotomy dash', False, 'WHITE')
+    
     pygame.display.update()
     clock.tick(60)
-    pass
-
-
-if event.type == pygame.MOUSEBUTTONDOWN:
-        if gdpb_rect.collidepoint(event.pos):
-            game_active = True
-if game_active == True:
-    screen = screen2
-    screen.blit(background, (0,0))
-    screen.blit(newlobotomy.png (300,130))
-    screen.blit()
-    screen.blit()
-    level_name = font2.render('Lobotomy dash', False, WHITE)
